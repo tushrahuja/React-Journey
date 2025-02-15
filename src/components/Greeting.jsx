102bc7324abeaa3323b8a4afecbe8fac32d5f2a8
tushrahuja <ahuja.tushar.m@gmail.com>
2025-02-15 21:15:45 +0530

    another conditional rendering example

---
+++ src/components/Greeting.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const today = new Date();
+
+const Greeting = ({timeOfDay}) => {
+  return timeOfDay === "morning" ? (<h1>Good Morning!</h1>) : (<h1>Good Afternoon!</h1>);
+}
+
+export default Greeting
