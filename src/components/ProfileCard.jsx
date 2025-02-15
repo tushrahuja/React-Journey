1eef31c7906d19d22059d862d090a7bc3ca92eec
tushrahuja <ahuja.tushar.m@gmail.com>
2025-02-15 21:08:25 +0530

    styling with css

---
+++ src/components/ProfileCard.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const ProfileCard = () => {
+  const styles = { backgroundColor: 'lightgrey', padding: '15px', borderRadius: '8px', textColor: 'black' }
+
+  return (
+    <div style={styles}>
+      <h1>Profile Card</h1>
+      <p>This is an profile card made by using css styles with the help of rendering the "styles" object.</p>
+    </div>
+  )
+}
+
+export default ProfileCard
---
+++ src/components/StyleCard.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const StyleCard = () => {
+  return (
+    <div 
+    style={{
+      backgroundColor: 'lightblue', 
+      padding: '20px', 
+      borderRadius: '10px', 
+      textColor: 'darkblue'
+    }} 
+    >
+      <h1>Style Card</h1>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci neque consectetur autem. Accusantium tenetur, odit aliquam suscipit, nesciunt et vitae, quo temporibus impedit omnis delectus veritatis voluptatum rem illum fugit saepe non.</p> 
+    </div>
+  )
+}
+
+export default StyleCard
